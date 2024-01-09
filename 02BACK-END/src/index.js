529e9a0a295affc8977fa789733ab6c4f3c603bb
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-01-10 01:08:22 +0530

    index entry file

--- 02BACK-END/src/index.js
+++ 02BACK-END/src/index.js
@@ -1,9 +1,25 @@
 import dotenv from "dotenv"
 import connectDB from "./db/DB_CONNECT.js"
+import app from "./app.js";
 
 dotenv.config({
     path: "./.env"
 })
   
 
-connectDB();
+connectDB()
+.then(() => {
+    // to check any error is there after db connection
+    app.on("error", (error) => {
+        console.log("Error : ", error);
+        throw error ;
+    });
+
+    // start the server 
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is listening at port: ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MONGO DB Connection Failed !!! ", error);
+});
