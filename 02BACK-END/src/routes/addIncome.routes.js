cfbdb477d74486915930d59effd4662f445ce8c6
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-01-11 17:27:03 +0530

    route file

--- 02BACK-END/src/routes/addIncome.routes.js
+++ 02BACK-END/src/routes/addIncome.routes.js
@@ -1,10 +1,19 @@
 import {Router} from "express"
 import { addIncome, getIncome, deleteIncome} from "../controllers/income.controller.js";
+import {addExpense, getExpense, deleteExpense} from "../controllers/expense.controller.js"
 
 const router = Router();
 
+// routes
+
+// Income routes
 router.route("/add-income").post(addIncome);
 router.route("/get-incomes").get(getIncome);
 router.route("/delete-income/:id").delete(deleteIncome);
+
+// Expense routes
+router.route("/add-expense").post(addExpense);
+router.route("/get-expenses").get(getExpense);
+router.route("/delete-expense/:id").delete(deleteExpense);
 
 export default router; 
