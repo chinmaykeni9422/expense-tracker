a13a12c279d65e56d5f5c0ff81c1021128707854
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-01-11 17:26:54 +0530

    expense model

---
+++ 02BACK-END/src/models/expense.model.js
@@ -0,0 +1,45 @@
+import mongoose , {Schema } from "mongoose"
+
+const expenseSchema = new Schema(
+    {
+        title:{
+            type: String,
+            required: true,
+            trim: true,
+            maxLenght: 50
+        },
+        amount:{
+            type: Number,
+            required: true,
+            trim: true,
+            maxLenght: 20
+        },
+        Type:{
+            type: String,
+            default:"expense"
+        },
+        date:{
+            type: Date,
+            required: true,
+            trim: true,
+        },
+        category:{
+            type: String,
+            required: true,
+            trim: true,
+        },
+        description:{
+            type: String,
+            required: true,
+            trim: true,
+            maxLenght: 20
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Expense = mongoose.model("Expense", expenseSchema);
+
+export default Expense;
