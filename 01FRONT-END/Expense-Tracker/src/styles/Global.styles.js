0eac791624f022432fa7a5207863aa007c5d128a
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-01-12 20:37:18 +0530

    style file

--- 01FRONT-END/Expense-Tracker/src/styles/Global.styles.js
+++ 01FRONT-END/Expense-Tracker/src/styles/Global.styles.js
@@ -1,1 +1,22 @@
-import {createGlobalStyle} from "styled-components"
+import {createGlobalStyle} from "styled-components";
+
+const GlobalStyle = createGlobalStyle `
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+    }
+
+    :root{
+        --primary-color: #222260;
+        --primary-color2: 'color: rgba(34, 34, 96, .6)';
+        --primary-color3: 'color: rgba(34, 34, 96, .4)';
+        --color-green: #42AD00;
+        --color-grey: #aaa;
+        --color-accent: #F56692;
+        --color-delete: #FF0000;
+    }
+`;
+
+export default GlobalStyle;
