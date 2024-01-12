17785281448bde525f352a9d02ade9e12f6d35d6
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-01-12 19:02:12 +0530

    user routes

--- 02BACK-END/src/routes/user.routes.js
+++ 02BACK-END/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import {registerUser, loginUser, logoutUser} from "../controllers/user.controller.js"
+import {registerUser, loginUser, logoutUser, refreshAccessToken} from "../controllers/user.controller.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -7,6 +7,9 @@ const router = Router();
 //user routes
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+
+// secured routes
 router.route("/logout").post(verifyJWT ,logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router; 
