7a4a8bd2469f47c7687f76cbee73ec3545417953
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-01-14 22:13:55 +0530

    income components

---
+++ 01FRONT-END/Expense-Tracker/src/components/Income/Income.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from "styled-components"
+import {InnerLayout} from "../../styles/Layouts.styles.js"
+
+function Income() {
+  return (
+    <IncomeStyled>
+        
+        <InnerLayout>
+
+            <h2>Income</h2>
+
+            <div className='income-content'>
+
+                <div className='form-container'>
+                    
+                </div>
+
+                <div className="incomes">
+                    
+                </div>
+
+            </div>
+
+        </InnerLayout>
+
+    </IncomeStyled>
+  )
+}
+
+const IncomeStyled = styled.div`
+
+`;
+
+export default Income;
