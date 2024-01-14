93cdbe8c31ec462205bb6aa5911f207fef9f3592
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-01-14 22:13:19 +0530

    navigation components

--- 01FRONT-END/Expense-Tracker/src/components/Navigation/Navigation.jsx
+++ 01FRONT-END/Expense-Tracker/src/components/Navigation/Navigation.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from "styled-components";
 import { menuItems } from '../../utils/MenuItem.js';
 import { signout } from '../../utils/Icons.jsx';
 
-function Navigation() {
+function Navigation({active, setActive}) {   
+
   return (
     <div>
         <NavStyled>
@@ -19,7 +20,7 @@ function Navigation() {
 
             <ul className="menu-items">
                 {menuItems.map((item) => {
-                    return <li key={item.id}>
+                    return <li className={active === item.id ? "active": ""} key={item.id} onClick={() => setActive(item.id)}>
                                 {item.icon}
                                 <span>{item.title}</span>
                             </li>
@@ -38,9 +39,67 @@ function Navigation() {
 }
 
 const NavStyled = styled.nav`
-padding: 2rem 1.5rem;
-width: 374px;
-height: 100%;  
+    padding: 2rem 1.5rem;
+    width: 374px;
+    height: 100%;  
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 2rem;
+    .user-con{
+        height: 100px;
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        h2{
+            color: rgba(34, 34, 96, 1);
+        }
+        p{
+            color: rgba(34, 34, 96, .6);
+        }
+    }
+    .menu-items{
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        li{
+            display: grid;
+            grid-template-columns: 40px auto;
+            align-items: center;
+            margin: .6rem 0;
+            font-weight: 500;
+            cursor: pointer;
+            transition: all .4s ease-in-out;
+            color: rgba(34, 34, 96, .6);
+            padding-left: 1rem;
+            position: relative;
+            i{
+                color: rgba(34, 34, 96, 0.6);
+                font-size: 1.4rem;
+                transition: all .4s ease-in-out;
+            }
+        }
+    }
+    .active{
+        color: rgba(34, 34, 96, 1) !important;
+        i{
+            color: rgba(34, 34, 96, 1) !important;
+        }
+        &::before{
+            content: "";
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 4px;
+            height: 100%;
+            background: #222260;
+            border-radius: 0 10px 10px 0;
+        }
+    }
 `;
 
 export default Navigation
