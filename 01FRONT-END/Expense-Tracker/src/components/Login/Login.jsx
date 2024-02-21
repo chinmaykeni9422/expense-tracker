d02efd841abb65d23755959c641be4617daed627
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-02-21 19:37:30 +0530

    log in component

---
+++ 01FRONT-END/Expense-Tracker/src/components/Login/Login.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import { InnerLayout } from '../../styles/Layouts.styles';
+
+function Login() {
+  return (
+    <LoginStyled>
+
+        <InnerLayout>
+
+            Login
+            
+        </InnerLayout>
+        
+    </LoginStyled>
+  )
+}
+
+const LoginStyled = styled.div`
+  
+`;
+
+export default Login
